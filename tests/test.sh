56aab99b79f78a33b2a6395ad2f1fa22829c3706
Rinoahu <xiaohu@iastate.edu>
2017-06-06 13:32:23 -0500

    Modify README.md

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,6 @@ debias -cprot 100 -i goa_yeast.gaf goa_dicty.gaf -a C -WCTHRESHp 2 -recal 1 -odi
 debias -i goa_yeast.gaf goa_dicty.gaf -a C P -PLTHRESHp 30 -e EXPEC IBA -single 1 -odir output1
 debias -cattn 1000 -i goa_yeast.gaf goa_dicty.gaf -a C P -einv COMPEC -pref testing -selrefinv Reactome -odir output2
 
-rm -rf output*
+rm -rf ./output* ./data
 #pip uninstall debias
 
