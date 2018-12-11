7c1839b14feb2cdf7d2caf4c38215ab753d2cc0c
parnaljoshi <44910482+parnaljoshi@users.noreply.github.com>
2018-12-11 15:42:02 -0600

    Update debias.py

--- lib/debias.py
+++ lib/debias.py
@@ -447,9 +447,6 @@ def assignProbabilitiesToOntologyTree( g, Prot_to_GO_Map, all_GO_Terms, ontology
         else:
             prob = num / denom
         ontology_to_ia_map[node] = [prob, -math.log( prob, 2 )]
-        fileTemp1 = open("PLIC.txt","a")
-        fileTemp1.write(node, "\t", ontology_to_ia_map[node])
-        fileTemp1.close()
 
 def assignProbabilitiesToOntologyGraphs( Prot_to_GO_Map, all_GO_Terms,aspects ):
     mf_g = cp.load( open( FILE_MFO_ONTOLOGY_GRAPH, "rb" ) )
@@ -461,6 +458,9 @@ def assignProbabilitiesToOntologyGraphs( Prot_to_GO_Map, all_GO_Terms,aspects ):
     assignProbabilitiesToOntologyTree( cc_g, Prot_to_GO_Map, all_GO_Terms, ontology_to_ia_map, 'CCO' )
     """for GO in ontology_to_ia_map:
         vprint(ontology_to_ia_map[GO])"""
+    fileTemp1 = open("PLIC.txt","a")
+    fileTemp1.write(ontology_to_ia_map[GO])
+    fileTemp1.close()
     return ontology_to_ia_map
 
 def calculateInformationAccretionForEachProtein( Prot_to_GO_Map, ontology_to_ia_map ):
