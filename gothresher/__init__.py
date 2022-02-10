23ef7b5154f7b0cd7597682606d3ad332c832204
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2022-02-09 18:31:36 -0600

    Add files via upload

---
+++ gothresher/__init__.py
@@ -0,0 +1,5 @@
+name = 'gothresher'
+
+#def main():
+#    """Entry point for the application script"""
+#    print("Call your main application code here")
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 """A setuptools based setup module.
-
 See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
@@ -18,49 +17,35 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='debias',
+    name='gothresher',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.165',
+    version='1.0.11',
 
     description='remove bias from GAF files',
-    long_description=long_description,
+    # long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/Rinoahu/debias',
+    url='https://github.com/FriedbergLab/debias',
 
     # Author details
-    author='xiao hu',
-    author_email='xiaohu@iastate.edu',
+    author='Parnal Joshi',
+    author_email='parnal@iastate.edu',
 
     # Choose your license
     license='GPLv3',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
+       # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        #'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.3',
-        #'Programming Language :: Python :: 3.4',
-        #'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
     ],
 
     # What does your project relate to?
@@ -70,46 +55,28 @@ setup(
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['networkx', 'matplotlib', 'numpy', 'Biopython', 'xlsxwriter'],
 
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    #extras_require={
-    #    'dev': ['check-manifest'],
-    #    'test': ['coverage'],
-    #},
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'sample': ['package_data.dat'],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
+    # package_dir={'': 'gothresher'},
+    entry_points={
+        'console_scripts': [
+            'gothresher = gothresher.gothresher:main',
+            'gothresher_prep = gothresher.gothresher_prep:main',
+        ]
+    },
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'debias=lib.debias:main',
-            'debias_prep=lib.debias_prep:main',
+    #entry_points={
+    #    'console_scripts': [
+    #        'gothresher = gothresher:main',
+    #        'gothresher_prep = gothresher_prep:main',
 
-        ],
-    },
+    #    ],
+    #},
 )
