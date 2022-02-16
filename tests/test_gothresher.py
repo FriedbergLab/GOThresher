c2971a8c8d18c75e76424f080d6aea9ba1bd8176
Pranav Khade <Pranavkhade@users.noreply.github.com>
2022-02-16 12:47:35 -0600

    unittest fix 10

--- tests/test_gothresher.py
+++ tests/test_gothresher.py
@@ -5,7 +5,7 @@ import os
 from Bio.UniProt import GOA
 from gothresher import gothresher
 
-input_file = '/example_data/goa_exampleYeast.gaf'
+input_file = 'example_data/goa_exampleYeast.gaf'
 # print(input_file)
 
 # Convert from GAF to required format
